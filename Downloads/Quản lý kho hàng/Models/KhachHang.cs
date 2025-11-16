d6663cb492053a990a25edd1a28e8797becfbff6
KhoaBun <128281841+khoa231@users.noreply.github.com>
2025-11-17 00:34:03 +0700

    update NhanVien done! mvc chạy được

---
+++ Downloads/Quản lý kho hàng/Models/KhachHang.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace QLKhoHang.Models
+{
+    public class KhachHang
+    {
+        [Key]
+        [StringLength(10)]
+        [BindNever]
+        public string MaKH { get; set; }
+
+        [Required(ErrorMessage = "Tên khách hàng không được để trống")]
+        [StringLength(100)]
+        public string TenKH { get; set; }
+
+        [StringLength(20)]
+        public string SDT { get; set; }
+
+        [StringLength(255)]
+        public string DiaChi { get; set; }
+
+        [BindNever]
+        public ICollection<PhieuXuat> PhieuXuat { get; set; }
+    }
+}
